a85c62711a4df0213884784dc771b3339a93583d
janskiba <jan.skiba@dkfz-heidelberg.de>
2023-11-28 15:45:43 +0000

    Remove  special chars from pw gen.
    Apparently some java libs dont handle this properly

--- central/src/keycloak.rs
+++ central/src/keycloak.rs
@@ -256,7 +256,7 @@ fn generate_secret() -> String {
     use rand::Rng;
     const CHARSET: &[u8] = b"ABCDEFGHIJKLMNOPQRSTUVWXYZ\
                             abcdefghijklmnopqrstuvwxyz\
-                            0123456789)(*&^%$#@!~";
+                            0123456789";
     const PASSWORD_LEN: usize = 30;
     let mut rng = rand::thread_rng();
 
