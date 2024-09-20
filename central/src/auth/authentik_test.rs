d9f5ee5bbab2827db03c57d55fbc6e3bb07a9cef
Martin Jurk <m049k@e260-mb007-wl.fritz.box>
2024-09-20 11:49:13 +0200

    test form x www urlencoded

--- central/src/auth/authentik_test.rs
+++ central/src/auth/authentik_test.rs
@@ -1,4 +1,5 @@
 use beam_lib::reqwest::{self, Error, StatusCode, Url};
+use serde::{Deserialize, Serialize};
 use serde_json::json;
 
 use crate::CLIENT;
@@ -7,23 +8,27 @@ use crate::CLIENT;
 #[tokio::test]
 async fn get_access_token() {
     let path_url = "http://localhost:9000/application/o/token";
-    #[derive(serde::Deserialize, Debug)]
+    #[derive(Deserialize, Serialize, Debug)]
     struct Token {
         access_token: String,
     }
-    let test = CLIENT
+    let response = CLIENT
         .post(path_url)
-        .form(&json!({
-            "grant_type": "client_credentials",
-            "client_id": "MI4DbeyktmjbXJRmUY9tkWvhK7yOzly139EgzhPZ",
-            "client_secret": "YGcFnXQMI7HqeDUWClhTkZmPtYj4aB2z3khnoMNpCo8CgTOhUqqOFE56dP2WOJoPGOeqdPsVCrR4yvjnJviYK6dY8WeykDqnzAO1xCLHOsPxefcSAa21qe0ru2bwWBi7",
-            "scope": "openid"
-        }))
+        .header("Content-Type", "applikation/x-www-form-urlencoded")
+        .form(&[
+            ("grant_type", "client_credentials"),
+            ("client_id", "MI4DbeyktmjbXJRmUY9tkWvhK7yOzly139EgzhPZ"),
+            ("client_secret", "YGcFnXQMI7HqeDUWClhTkZmPtYj4aB2z3khnoMNpCo8CgTOhUqqOFE56dP2WOJoPGOeqdPsVCrR4yvjnJviYK6dY8WeykDqnzAO1xCLHOsPxefcSAa21qe0ru2bwWBi7"),
+            ("scope", "openid")
+        ])
         .send()
         .await
-        .unwrap()
+        .expect("no response");
+
+    let t = response
         .json::<Token>()
         .await
-        .unwrap();
-    dbg!(test);
+        .expect("Token can not be parseed");
+    dbg!(&t);
+    assert!(!t.access_token.is_empty());
 }
