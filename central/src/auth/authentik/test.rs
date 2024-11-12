b4d2bc46074d6f0c6bb6b6d424a035b461ac7717
Martin Jurk <m049k@e260-mb007-wl.local>
2024-11-12 14:29:10 +0100

    failt test

--- central/src/auth/authentik/test.rs
+++ central/src/auth/authentik/test.rs
@@ -1,17 +1,36 @@
+use crate::auth::authentik::app::generate_app_values;
+use crate::auth::authentik::group::create_groups;
+use crate::auth::authentik::{
+    app_configs_match, combine_app_provider, compare_applications, get_application, get_uuid,
+    AuthentikConfig,
+};
+use crate::CLIENT;
 use beam_lib::reqwest::{self, Error, StatusCode, Url};
 use serde::{Deserialize, Serialize};
 use serde_json::json;
 use shared::{OIDCConfig, SecretResult};
-use crate::auth::authentik::app::generate_app_values;
-use crate::auth::authentik::group::create_groups;
-use crate::auth::authentik::{app_configs_match, combine_app_provider, compare_applications, get_application, get_uuid, AuthentikConfig};
-use crate::CLIENT;
 
 #[derive(Deserialize, Serialize, Debug)]
 struct Token {
     access_token: String,
 }
 
+#[cfg(test)]
+async fn setup_authentik() -> reqwest::Result<(String, AuthentikConfig)> {
+    //let token = get_access_token_via_admin_login().await?;
+    let token = Token {
+        access_token: "NXChuwYcHBf4ggcg1VdWdwKaEgqNxwl07nWLSnBsAK27YHNTdi0z45K6Ioun".to_owned(),
+    };
+    Ok((
+        token.access_token,
+        AuthentikConfig {
+            authentik_url: "http://localhost:9000".parse().unwrap(),
+            authentik_id: "unused in tests".into(),
+            authentik_secret: "unused in tests".into(),
+            authentik_groups_per_bh: vec!["DKTK_CCP_#".into(), "DKTK_CCP_#_Verwalter".into()],
+        },
+    ))
+}
 
 #[ignore = "Requires setting up a authentik"]
 #[tokio::test]
@@ -45,7 +64,11 @@ async fn get_access_token_via_admin_login() -> reqwest::Result<String> {
     CLIENT
         .post(&format!(
             "{}/application/o/token/",
-            if cfg!(test) { "http://localhost:9000"} else { "http://authentik:8080" }
+            if cfg!(test) {
+                "http://localhost:9000"
+            } else {
+                "http://authentik:8080"
+            }
         ))
         .form(&json!({
             "client_id": "admin-cli",
@@ -60,66 +83,106 @@ async fn get_access_token_via_admin_login() -> reqwest::Result<String> {
         .map(|t| t.access_token)
 }
 
-#[cfg(test)]
-async fn setup_authentik() -> reqwest::Result<(String, AuthentikConfig)> {
-    //let token = get_access_token_via_admin_login().await?;
-    let token = Token{
-        access_token: "iLAxhzywZhZUHKyCuk2ZPRDFxGJYNml0oTs78qF82kTlUyp5JRGVc4UrL2V8".to_owned()
-    };
-    Ok((
-        token.access_token,
-        AuthentikConfig {
-            authentik_url: "http://localhost:9000".parse().unwrap(),
-            authentik_id: "unused in tests".into(),
-            authentik_secret: "unused in tests".into(),
-            authentik_groups_per_bh: vec!["DKTK_CCP_#".into(), "DKTK_CCP_#_Verwalter".into()],
-        },
-    ))
-}
-
-#[ignore = "Requires setting up a authentik"]
+//#[ignore = "Requires setting up a authentik"]
 #[tokio::test]
 async fn test_create_client() -> anyhow::Result<()> {
     let (token, conf) = setup_authentik().await?;
-    let name = "home";
+    let name = "office";
     // public client
-    let client_config = OIDCConfig { is_public: true, redirect_urls: vec!["http://foo/bar".into()] };
-    let (SecretResult::Created(pw) | SecretResult::AlreadyExisted(pw)) = dbg!(combine_app_provider(&token, name, &client_config, &conf).await?) else {
+    let client_config = OIDCConfig {
+        is_public: true,
+        redirect_urls: vec!["http://foo/bar".into()],
+    };
+    let (SecretResult::Created(pw) | SecretResult::AlreadyExisted(pw)) =
+        dbg!(combine_app_provider(&token, name, &client_config, &conf).await?)
+    else {
         panic!("Not created or existed")
     };
-    let c = dbg!(get_application(name, &token, &client_config, &conf).await.unwrap());
-    assert!(app_configs_match(&c, &generate_app_values(name, name, &client_config)));
-    assert!(dbg!(compare_applications(&token, name, &client_config, &conf).await?));
+    let c = dbg!(get_application(name, &token, &client_config, &conf)
+        .await
+        .unwrap());
+    assert!(app_configs_match(
+        &c,
+        &generate_app_values(name, name, &client_config)
+    ));
+    assert!(dbg!(
+        compare_applications(&token, name, &client_config, &conf).await?
+    ));
 
     // private client
-    let client_config = OIDCConfig { is_public: false, redirect_urls: vec!["http://foo/bar".into()] };
-    let (SecretResult::Created(pw) | SecretResult::AlreadyExisted(pw)) = dbg!(combine_app_provider(&token, name, &client_config, &conf).await?) else {
+    let client_config = OIDCConfig {
+        is_public: false,
+        redirect_urls: vec!["http://foo/bar".into()],
+    };
+    let (SecretResult::Created(pw) | SecretResult::AlreadyExisted(pw)) =
+        dbg!(combine_app_provider(&token, name, &client_config, &conf).await?)
+    else {
         panic!("Not created or existed")
     };
-    let c = dbg!(get_application(name, &token, &client_config, &conf).await.unwrap());
-    assert!(app_configs_match(&c, &generate_app_values(name, name, &client_config)));
-    assert!(dbg!(compare_applications(&token, name, &client_config, &conf).await?));
+    let c = dbg!(get_application(name, &token, &client_config, &conf)
+        .await
+        .unwrap());
+    assert!(app_configs_match(
+        &c,
+        &generate_app_values(name, name, &client_config)
+    ));
+    assert!(dbg!(
+        compare_applications(&token, name, &client_config, &conf).await?
+    ));
 
     Ok(())
 }
 
-#[ignore = "Requires setting up a authentik"]
+//#[ignore = "Requires setting up a authentik"]
 #[tokio::test]
 async fn group_test() -> anyhow::Result<()> {
     let (token, conf) = setup_authentik().await?;
-    create_groups("e", &token, &conf).await?;
-    // dbg!(get_realm_permission_roles(&token, &conf).await?);
-    // add_service_account_roles(&token, "test-private", &conf).await?;
-    Ok(())
+    create_groups("team", &token, &conf).await
 }
 
-#[ignore = "Requires setting up a authentik"]
+// #[ignore = "Requires setting up a authentik"]
 #[tokio::test]
-async fn test_flow_property() {
-    let (token, conf) = setup_authentik().await.expect("Cannot setup authentik as test");
-    let test_key = "groups";
+async fn test_flow() {
+    let (token, conf) = setup_authentik()
+        .await
+        .expect("Cannot setup authentik as test");
+    let test_key = "authorization_flow";
     let flow_url = "/api/v3/flows/instances/?ordering=slug&page=1&page_size=20&search=";
     let res = get_uuid(flow_url, &conf, &token, test_key).await;
-        dbg!(res);
-    
+    dbg!(res);
+}
+
+// #[ignore = "Requires setting up a authentik"]
+#[tokio::test]
+async fn test_property() {
+    let (token, conf) = setup_authentik()
+        .await
+        .expect("Cannot setup authentik as test");
+    let test_key = "web-origins";
+    let flow_url = "/api/v3/propertymappings/all/?managed__isnull=true&ordering=name&page=1&page_size=20&search=groups";
+    let res = get_uuid(flow_url, &conf, &token, test_key).await;
+    dbg!(res);
+}
+
+//#[ignore = "Requires setting up a authentik"]
+#[tokio::test]
+async fn create_property() {
+    let (token, conf) = setup_authentik()
+        .await
+        .expect("Cannot setup authentik as test");
+    let acr = "test".to_owned();
+    let ext = "return{}".to_owned();
+    let json_property = json!({
+    "name": acr,
+    "expression": ext
+    });
+    let propperty_url = "/api/v3/propertymappings/source/oauth/";
+    let res = CLIENT
+        .post(&format!("{}{}", conf.authentik_url, propperty_url))
+        .bearer_auth(token)
+        .json(&json_property)
+        .send()
+        .await
+        .expect("no response");
+    dbg!(res);
 }
