551f51badd0033b8e3e75f818908ea6c5d522458
Martin Jurk <96107909+Martin1088@users.noreply.github.com>
2025-01-28 09:41:21 +0100

    Update central/src/config.rs
    Co-authored-by: Jan <59206115+Threated@users.noreply.github.com>

--- central/src/config.rs
+++ central/src/config.rs
@@ -12,10 +12,6 @@ use crate::auth::{
 /// Central secret sync
 #[derive(Debug, Parser)]
 pub struct Config {
-    /// Address the server should bind to
-    #[clap(env, long, default_value = "0.0.0.0:8080")]
-    pub bind_addr: SocketAddr,
-
     /// Url of the local beam proxy which is required to have sockets enabled
     #[clap(env, long, default_value = "http://beam-proxy:8081")]
     pub beam_url: Url,
