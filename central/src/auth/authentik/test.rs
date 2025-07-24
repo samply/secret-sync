78e8215752e27ecb7b451598aee0c084245ca314
Martin Jurk <m049k@vpngate1240.inet.dkfz-heidelberg.de>
2025-07-24 13:33:10 +0200

    default values changed

--- central/src/auth/authentik/test.rs
+++ central/src/auth/authentik/test.rs
@@ -37,8 +37,8 @@ pub fn setup_authentik() -> reqwest::Result<AuthentikConfig> {
             "Local Account".into(),
         ],
         authentik_flow_auth: "Authorize Application".into(),
-        authentik_crypto_signing_key: "Logged out of application".into(),
-        authentik_flow_invalidation: "authentik Self-signed Certificate".into(),
+        authentik_crypto_signing_key: "authentik Self-signed Certificate".into(),
+        authentik_flow_invalidation: "Logged out of application".into(),
     })
 }
 
